9d676bf01701053ea8b667ed0d034515dabb7f03
dilkhush4707 <dilkhushjangid4707@gmail.com>
2024-09-06 21:35:12 +0530

    third step

--- TODO.js
+++ TODO.js
@@ -1,20 +1,30 @@
-let todoList = [];
+let todoList = [
+  { item: "buy milk", duedate: "4/04/2023" },
+  { item: "Go to college", duedate: "4/04/2023" },
+];
+
 function addTodo() {
-  let todo_date = document.querySelector("#input_date");
-  let todo_element = document.querySelector("#input_task");
-  let todo_item = todo_element.value || "invalid value delete";
-  todoList.push(todo_item);
+  let inputElement = document.querySelector("#input_task");
+  let dateElement = document.querySelector("#input_date");
+  let todo_item = inputElement.value || "invalid value ";
+  let todo_date = dateElement.value || " delete this";
+
+  todoList.push({ item: todo_item, duedate: todo_date });
   displayItems();
-  todo_element.value = null;
+  inputElement = null;
+  dateElement = null;
 }
 function displayItems() {
   let containerElement = document.querySelector(".todo-container");
   let newHtml = "";
+
   let LENGTH = todoList.length;
   for (let i = 0; i <= LENGTH - 1; i++) {
+    let { item, duedate } = todoList[i];
     newHtml += `
     <div class="todo_item_container">
-    <span>${todoList[i]}</span>
+    <span>${item}</span>
+    <span>${duedate}</span>
     <button onclick="todoList.splice(${i},1);displayItems();">
     Delete</button>
   </div>`;
